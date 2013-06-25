2d749ef62a8bd3a5e3fe0f441995702ebf44de78
Steve Lounsbury <steve.lounsbury@gmail.com>
2013-06-24 20:53:57 -0400

    Turn on cache busting and css minify

--- config.rb
+++ config.rb
@@ -46,21 +46,21 @@
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
+
+ignore 'README.md'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
